3cb63f11595d8128ffeefd4e651ac9d217360cea
MikeKhay <khajmike@gmail.com>
2026-01-04 10:19:58 +0200

    task

--- hybris/bin/custom/training/trainingcore/src/org/training/core/training/task/TrainingTaskService.java
+++ hybris/bin/custom/training/trainingcore/src/org/training/core/training/task/TrainingTaskService.java
@@ -29,6 +29,44 @@ public class TrainingTaskService {
 
         // schedule
         taskService.scheduleTask(task);
+
+
+
+
+
+//                  EVENTS IN TASK
+//        TaskConditionModel cond = modelService.create(TaskConditionModel.class);
+//
+//                    configure them
+//        task.setRunnerBean("MyRunner");
+//                    define event name
+//        cond.setUniqueID("MyEventArrived");
+//                    add to task
+//        task.setConditions( Collections.singleton( cond ) );
+//
+//                    schedule
+//        taskService.scheduleTask(task);
+
+
+
+//        TaskService taskService = ...
+
+//                    trigger the event
+//        taskService.triggerEvent( "MyEventArrived" );
+
+
+
+
+//                  CONDITIONS AND TIMEOUTS IN TASK
+//        TaskConditionModel cond1 = modelService.create(TaskConditionModel.class);
+//        TaskConditionModel cond2 = modelService.create(TaskConditionModel.class);
+//
+//                   set single expiration date for the whole task: now + 10min
+//        task.setExpirationDate(new Date( System.currentTimeMillis() + (10 * 60 * 1000) ))
+//
+//                   set expiration date for conditions
+//        cond1.setExpirationDate(new Date( System.currentTimeMillis() + (2 * 60 * 1000) ))
+//        cond2.setExpirationDate(new Date( System.currentTimeMillis() + (6 * 60 * 1000) ))
     }
 
     @Required
