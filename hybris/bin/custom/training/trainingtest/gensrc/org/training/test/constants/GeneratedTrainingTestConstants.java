45df276f05a545387ce2c692648bc811d569e194
MikeKhay <khajmike@gmail.com>
2026-01-11 08:53:29 +0200

    update .gitignore, add hybris/config folder, add Jalo files

---
+++ hybris/bin/custom/training/trainingtest/gensrc/org/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 7, 2026, 10:51:08 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
